0545c1052de10eec68aa88bcaef01b55fb1588d3
Hongin Lim <96184680+Hongin-Lim@users.noreply.github.com>
2022-05-24 11:36:32 +0900

    setting.py 수정

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a(8hsa+f)-_-pmvd*kz2ya^t4tt7%i=$s@*^(ofvtv_*h#&w7@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
